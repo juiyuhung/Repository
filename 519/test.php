3a9f231c800d2d07511fec6d23180c3937f6dcbd
w2571972 <w2571972@gmail.com>
2016-11-25 03:12:48 -0600

    Add files via upload

---
+++ 519/test.php
@@ -0,0 +1,233 @@
+<!DOCTYPE HTML>
+<!--
+	Prologue by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>計算機中心-工作日誌表</title>
+		<link rel="icon" href="images/favicon.ico"/>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+	</head>
+	<body>
+
+		<!-- Header -->
+			<div id="header">
+
+				<div class="top">
+
+					<!-- Logo -->
+						<div id="logo">
+							<!--span class="image avatar48"><img src="images/avatar.jpg" alt="" /></span-->
+							<h1 id="title">User</h1>
+							<p>計中工作</p>
+						</div>
+
+					<!-- Nav -->
+						<nav id="nav">
+							<!--
+
+								Prologue's nav expects links in one of two formats:
+
+								1. Hash link (scrolls to a different section within the page)
+
+								   <li><a href="#foobar" id="foobar-link" class="icon fa-whatever-icon-you-want skel-layers-ignoreHref"><span class="label">Foobar</span></a></li>
+
+								2. Standard link (sends the user to another page/site)
+
+								   <li><a href="http://foobar.tld" id="foobar-link" class="icon fa-whatever-icon-you-want"><span class="label">Foobar</span></a></li>
+								   <span class="icon fa-home">  class="icon fa-th"  class="icon fa-user"   class="icon fa-envelope"
+			
+							-->
+							<ul>
+								<li><a href="#top" id="top-link" class="skel-layers-ignoreHref">Intro<span></span></a></li>
+								<li><a href="#portfolio" id="portfolio-link" class="skel-layers-ignoreHref"><span >08:00</span></a></li>
+								<li><a href="#about" id="about-link" class="skel-layers-ignoreHref"><span >09:00 </span></a></li>
+								<li><a href="#contact" id="contact-link" class="skel-layers-ignoreHref"><span>10:00 </span></a></li>
+							</ul>
+						</nav>
+
+				</div>
+
+				<!--div class="bottom">
+
+					<!-- Social Icons -->
+						<!--ul class="icons">
+							<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+							<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+							<li><a href="#" class="icon fa-github"><span class="label">Github</span></a></li>
+							<li><a href="#" class="icon fa-dribbble"><span class="label">Dribbble</span></a></li>
+							<li><a href="#" class="icon fa-envelope"><span class="label">Email</span></a></li>
+						</ul-->
+
+				<!--/div-->
+
+			</div>
+
+		<!-- Main -->
+			<div id="main">
+
+				<!-- Intro -->
+					<section id="top" class="one dark cover">
+						<div class="container">
+
+							<header>
+								<h2 class="alt">Hi! I'm <strong>Prologue</strong>, a <a href="http://html5up.net/license">free</a> responsive<br />
+								site template designed by <a href="http://html5up.net">HTML5 UP</a>.</h2>
+								<p>Ligula scelerisque justo sem accumsan diam quis<br />
+								vitae natoque dictum sollicitudin elementum.</p>
+							</header>
+
+							<footer>
+								<a href="#portfolio" class="button scrolly">Magna Aliquam</a>
+							</footer>
+
+						</div>
+					</section>
+
+				<!-- Portfolio -->
+					<section id="portfolio" class="two">
+						<div class="container">
+
+							<!--header>
+								<h2>工作內容</h2>
+							</header>
+							<p></p-->
+							
+							<table style="border:3px #cccccc solid;" border="1" cellpadding="10">
+								<thead>
+									<tr>
+										<th>工作內容</th>
+                                        <th colspan="2"> 處理狀況說明</th>
+                                        <th>負責人</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<td colspan = "3">
+                                            <div>
+                                            開門(至計中拿鑰匙；主顧318簽到) 先解除保全(316、318、322、324)
+                                            </div>
+                                        </td>
+                                        <td></td>
+									</tr>
+									<tr >
+                                        <td rowspan = "2"  >
+                                            <div>
+                                                開電腦螢幕、印表機、教室空調、電燈、窗簾等環境檢查
+                                            </div>
+                                            <div>
+                                                檢查課表與上機刷卡設定
+                                            </div>
+                                            <div>
+                                                廣播教學系統檢查
+                                            </div>
+                                        <td >主顧316</td>
+                                        <td >主顧320</td>
+										<td ></td>
+                                    </tr>
+                                    <tr>
+                                        <td >主顧322</td>
+                                        <td >主顧324</td>
+                                        <td ></td>
+                                    </tr>
+								</tbody>
+							</table>
+							
+						</div>
+					</section>
+
+				<!-- About Me -->
+					<section id="about" class="three">
+						<div class="container">
+							<table style="border:3px #cccccc solid;" border="1" cellpadding="10">
+								<thead>
+										<tr>
+											<th>工作內容</th>
+											<th colspan="2"> 處理狀況說明</th>
+											<th>負責人</th>
+										</tr>
+								</thead>
+								<tbody>
+								<tr >
+										<td rowspan = "3">
+											<div>
+												檢查318印表機紙張、碳粉匣正常與否
+											</div>
+											<div>
+													檢查印表機紙張、碳粉匣
+											</div>
+											<div>
+													分別在置放耗材處的登記本上確實記錄
+											</div>
+											<div>
+												檢查過後，右邊勾選確認
+											</div>
+										</td>
+										<td colspan="2" >黑白 No.1(紙張：　碳粉： )</td>
+										<td ></td>
+									</tr>
+									<tr >
+										<td colspan = "2">A4 務必在置放耗材處的登記本上確實記錄</td>
+										<td></td>
+									</tr>
+									<tr >
+										<td colspan = "2">Lexmark 務必在置放耗材處的登記本上確實記錄</td>
+										<td></td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
+					</section>
+
+				<!-- Contact -->
+					<section id="contact" class="four">
+						<div class="container">
+
+							<header>
+								<h2>Contact</h2>
+							</header>
+
+							<p>Elementum sem parturient nulla quam placerat viverra
+							mauris non cum elit tempus ullamcorper dolor. Libero rutrum ut lacinia
+							donec curae mus. Eleifend id porttitor ac ultricies lobortis sem nunc
+							orci ridiculus faucibus a consectetur. Porttitor curae mauris urna mi dolor.</p>
+
+							<form method="post" action="#">
+								<div class="row">
+									<div class="6u 12u$(mobile)"><input type="text" name="name" placeholder="Name" /></div>
+									<div class="6u$ 12u$(mobile)"><input type="text" name="email" placeholder="Email" /></div>
+									<div class="12u$">
+										<textarea name="message" placeholder="Message"></textarea>
+									</div>
+									<div class="12u$">
+										<input type="submit" value="Send Message" />
+									</div>
+								</div>
+							</form>
+
+						</div>
+					</section>
+
+			</div>
+
+		<!-- Footer -->
+			
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.scrollzer.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
